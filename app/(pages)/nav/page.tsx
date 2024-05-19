c31295218616f3abd3e053971251c1db33a835a8
leihuazhe <295482300@qq.com>
2024-05-19 09:06:20 +0800

    feat(140-7): Reserved, time:  2024-05-19 09:06:19

--- app/(pages)/nav/page.tsx
+++ app/(pages)/nav/page.tsx
@@ -1,12 +1,12 @@
 import { Sidebar } from "@/components/sidebar"
 import { SiteFooter } from "@/components/site-footer"
 import { LinkContent } from "@/components/link-content"
-import getNavLinks from "@/app/links";
+import getNavLinks, { CategoryWithLinks } from "@/app/links";
 
 // export const revalidate = 24 * 60 * 60;
 
 export default async function IndexPage() {
-  const navResources = await getNavLinks();
+  const navResources: CategoryWithLinks = await getNavLinks();
   const navItems = navResources.map(n => {
     return {
       title: n.title,
